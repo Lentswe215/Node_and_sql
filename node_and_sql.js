a68c356b4a6c5c84f40b7210d1c4c21c4d1aab06
Lentswe215 <ephraimmamonong@gmail.com>
2020-03-19 10:46:51 +0200

    made changes

--- node_and_sql.js
+++ node_and_sql.js
@@ -5,13 +5,53 @@ let client = new Client({
   password: "pass",
   host: "localhost",
   port: 5432,
-  database: "umuzi"
+  database: "visitordb"
 })
 
-client.connect()
-  .then(() => console.log("Connected successfully"))
-  .then(()=> client.query("Insert into employees(Name, Surname) values($1, $2)", ["Ephraim","Mamonong"]))
-  .then(()=> client.query("select * from employees"))
-  .then(results => console.table(results.rows))
-  .catch(e => console.log(e))
-  .finally(() => client.end())
+class Visitors {
+  constructor(fullName, age, dateOfVisit, timeOfVisit, assistedBy, comments){
+    this.fullName = fullName;
+    this.age = age;
+    this.dateOfVisit = dateOfVisit;
+    this.timeOfVisit = timeOfVisit;
+    this.assistedBy = assistedBy;
+    this.comments = comments;
+  }
+
+  addNewVisitor(){
+    client.connect()
+    .then(()=> client.query("INSERT into visitors(fullname, visitorage, dateofvisit, timeofvisit, assistedby, comments) values ($1,$2,$3,$4,$5,$6)",[this.fullName, this.age, this.dateOfVisit, this.timeOfVisit, this.assistedBy, this.comments]))
+    .then(()=> console.log("Visitor successfully saved"))
+    .catch((e) => console.log(e))
+    .finally(() => client.end())
+  }
+
+  listAllVisitors() {
+    client.connect()
+    .then(() => client.query("SELECT * from visitors"))
+    .then(results => console.table(results.rows))
+    .catch((e) => console.log(e))
+    .finally(()=> client.end())
+  }
+
+  deleteVisitor() {
+    client.connect()
+    .then(()=> client.query("DELETE from visitors WHERE fullname = $1", [this.fullName]))
+    .then(()=> console.log("Visitor successfully deleted"))
+    .catch((e) => console.log(e))
+    .finally(()=> client.end())
+  }
+
+}
+let visitor = new Visitors();
+let visitor2 = new Visitors("Ephraim Mamonong", 25, "12-07-2019", "10:42","Gomolemo Josephs", "It is a great place with wonderful view of the ocean and there is lots of fresh air around")
+
+visitor.listAllVisitors()
+// visitor2.deleteVisitor()
+
+// client.connect()
+//   .then(() => console.log("Connected successfully"))
+//   .then(()=> client.query("select * from employees"))
+//   .then(results => console.table(results.rows))
+//   .catch(e => console.log(e))
+//   .finally(() => client.end())
